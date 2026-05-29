3f85826f95ec950a283f6e6d213c411e56e0e27d
Manmeet Singh <87306390+ManmeetSingh122@users.noreply.github.com>
2026-05-30 03:14:34 +0530

    Update API version in import statement

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { fetchTMDB, endpoints, IMAGE_BASE_URL, SMALL_IMAGE_URL, getGenreNames } from './api.js?v=9006';
+import { fetchTMDB, endpoints, IMAGE_BASE_URL, SMALL_IMAGE_URL, getGenreNames } from './api.js?v=9011';
 
 const FALLBACK_IMAGE = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="1200" height="675" viewBox="0 0 1200 675"%3E%3Cdefs%3E%3ClinearGradient id="g" x1="0" x2="1" y1="0" y2="1[...]
 
